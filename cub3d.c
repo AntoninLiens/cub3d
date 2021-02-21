7bd31160515fb4cc9358ebdad1609d3fe2994188
Antonin Liens <aliens@mi-r3-p4.s19.be>
2021-02-21 14:50:13 +0100

    catch the map

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: aliens <aliens@students.s19.be>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/07 14:58:11 by aliens            #+#    #+#             */
-/*   Updated: 2021/02/21 13:33:10 by aliens           ###   ########.fr       */
+/*   Updated: 2021/02/21 14:48:22 by aliens           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,25 @@ void	init_params(void)
 void	init_map(char **argv)
 {
 	int		i;
+	int		j;
 	int		fd;
 	char	*line;
+	t_list	*charmap;
 	t_map	map;
 
 	i = 0;
+	j = -1;
+	charmap = NULL;
 	if (!(fd = open(argv[1], O_RDONLY)))
 		return ;
 	while (get_next_line(fd, &line) && i < 8)
 		i += get_infos_map(line, &map);
-	printf("i = %d\n", i);
+	while (get_next_line(fd, &line))
+	{
+		charmap = ft_lstnew(line);
+		printf("%s\n", charmap->content);
+		charmap = charmap->next;
+	}
 	printf("%d\n%d\n%d\n%d\n", map.r1, map.r2, map.f, map.c);
 	printf("%s\n%s\n%s\n%s\n%s\n", map.so, map.we, map.no, map.ea, map.s);
 	close(fd);
